3f69583f1106f5ff3d6424683b1b867387e482e0
Florian Serraille <florianserraille@gmail.com>
2022-12-20 10:19:14 -0300

    Add google chrome to install_dependencies.sh

--- .local/bin/install_dependencies.sh
+++ .local/bin/install_dependencies.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-sudo dnf install -y vim feh
+sudo dnf update
+
+# install core tools
+sudo dnf install -y vim tree wget curl tree
+
+# add workstation repository
+sudo dnf install -y fedora-workstation-repositories
+
+# install google chrome
+sudo dnf config-manager --set-enabled google-chrome
+sudo dnf install -y google-chrome-stable
